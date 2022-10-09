05df131a2823adccd994a93d756db070460cb675
Juan Cardona <juan_cardona@epam.com>
2022-10-09 07:42:06 -0500

    Adding some method references. It is not yet completed

--- src/main/java/com/epam/rd/elevate/gpr/commandlambdastream/Main.java
+++ src/main/java/com/epam/rd/elevate/gpr/commandlambdastream/Main.java
@@ -49,38 +49,50 @@ public class Main {
         return result;
     }
 
+    public static Boolean predicateRegion(SalesSummaryRow salesSummaryRow, Region r2) {
+        return salesSummaryRow.getRegion() == r2;
+    }
+
+    public static Predicate<SalesSummaryRow> createPredicateRegion(Region region) {
+        return (s -> s.getRegion() == region);
+    }
+
+    public static Predicate<SalesSummaryRow> createPredicateSalesCost(Double percent) {
+        return s -> (s.getUnitSalesValue() / s.getUnitCosts()) > percent;
+    }
+
     final static double THIRTEN_PERCENT = 0.30;
 
     public static void main(String[] args) {
         List<SalesSummaryRow> dbSales = DatabaseHelper.getDBSales();
 
-        forEach(dbSales,s -> System.out.println(s));
+        forEach(dbSales,System.out::println);
 
         System.out.println("\nNorth Sales Summary");
-        forEach(filter(dbSales, s -> s.getRegion() == Region.NORTH),
-                s -> System.out.println(s));
+        forEach(filter(dbSales, createPredicateRegion(Region.NORTH)),
+                System.out::println);
 
         System.out.println("\nSouth Sales Summary");
-        forEach(filter(dbSales, s -> s.getRegion() == Region.SOUTH),
+        forEach(filter(dbSales, createPredicateRegion(Region.SOUTH)),
                 s -> System.out.println(s));
 
         System.out.println("\nStarted units from West Sales Summary");
         forEach(map(filter(dbSales,
-                           s -> s.getRegion() == Region.WEST),
-                    s -> s.getInitialUnits()),
-                s -> System.out.println(s));
+                           createPredicateRegion(Region.WEST)),
+                    SalesSummaryRow::getInitialUnits),
+                System.out::println);
 
         System.out.println("\nFinals units from East Sales Summary");
         forEach(map(filter(dbSales,
-                           s -> s.getRegion() == Region.WEST),
-                    s -> s.finalUnits()),
-                s -> System.out.println(s));
+                           createPredicateRegion(Region.EAST)),
+                    SalesSummaryRow::finalUnits),
+                System.out::println);
 
         System.out.println("\nList of profits over 30 percent over the sales' cost.");
         forEach(map(filter(dbSales,
-                           s -> (s.getUnitSalesValue() / s.getUnitCosts()) > THIRTEN_PERCENT),
-                    s -> s.profit()),
-                s -> System.out.println(s));
+                           createPredicateSalesCost(THIRTEN_PERCENT)),
+                    SalesSummaryRow::profit),
+                System.out::println);
 
         System.out.println("Profit: " + reduce(dbSales,
                                                (s,r) -> s.profit() + r,
