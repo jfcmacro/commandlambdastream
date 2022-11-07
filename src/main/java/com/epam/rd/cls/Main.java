bebee0e2e8b8e24d4c92211d6d83e14b6a2a75cd
Juan Cardona <Juan_Cardona@epam.com>
2022-11-07 16:47:35 -0500

    Change package name and file name (DatabaseHelper DBHelper)

--- src/main/java/com/epam/rd/cls/Main.java
+++ src/main/java/com/epam/rd/cls/Main.java
@@ -1,4 +1,4 @@
-package com.epam.rd.elevate.gpr.commandlambdastream;
+package com.epam.rd.cls;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -52,7 +52,7 @@ public class Main {
     final static double THIRTEN_PERCENT = 0.30;
 
     public static void main(String[] args) {
-        List<SalesSummaryRow> dbSales = DatabaseHelper.getDBSales();
+        List<SalesSummaryRow> dbSales = DBHelper.getDBSales();
 
         forEach(dbSales,s -> System.out.println(s));
 
